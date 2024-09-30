01ffe345752a7a1a1ee86d95ed2bc5448b5049b5
Starbow-Break <phsyobs@naver.com>
2024-09-30 21:03:12 +0900

    Refactor : 스위치 코드 리팩토링

---
+++ Assets/Scripts/OnOffSwitch.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class OnOffSwitch : SpotlightSwitch
+{
+    [SerializeField] Sprite onSprite;
+    [SerializeField] Sprite offSprite;
+
+    protected override void Awake() {
+        base.Awake();
+
+        if(spotlight.isOn) {
+            sr.sprite = onSprite;
+        }
+        else {
+            sr.sprite = offSprite;
+        }
+    }
+
+    void Update() {
+        if(isInteracting && Input.GetKeyDown(KeyCode.Z)) {
+            ChangeSprite();
+            spotlight.Switch();
+        }
+    }
+
+    void ChangeSprite()
+    {
+        sr.sprite = sr.sprite == onSprite ? offSprite : onSprite;
+    }
+}
--- Assets/Scripts/RotateSwitch.cs
+++ Assets/Scripts/RotateSwitch.cs
@@ -2,48 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RotateSwitch : MonoBehaviour
+public class RotateSwitch : SpotlightSwitch
 {
-    [SerializeField] Spotlight spotlight;
     [SerializeField] Sprite[] sprites;
     [SerializeField] int startIndex = 0;
-
     int currentIndex;
 
-    bool isInteracting = false;
-    SpriteRenderer sr;
-
-    void Awake()
+    protected override void Awake()
     {
-        sr = GetComponent<SpriteRenderer>();
+        base.Awake();
+        
         currentIndex = startIndex;
-
         sr.sprite = sprites[currentIndex];
     }
 
     void Update() {
         if(isInteracting && Input.GetKeyDown(KeyCode.Z)) {
-            Switch();
+            ChangeSprite();
             spotlight.RotateSwitch();
         }
     }
 
-    void Switch()
+    void ChangeSprite()
     {
         currentIndex = (currentIndex + 1) % sprites.Length;
         sr.sprite = sprites[currentIndex];
-    }
-
-    void OnTriggerEnter2D(Collider2D other) {
-        if(!isInteracting && other.gameObject.CompareTag("Player")) {
-            isInteracting = true;
-        }
-    }
-
-    void OnTriggerExit2D(Collider2D other)
-    {
-        if(isInteracting && other.gameObject.CompareTag("Player")) {
-            isInteracting = false;
-        }
     }
 }
