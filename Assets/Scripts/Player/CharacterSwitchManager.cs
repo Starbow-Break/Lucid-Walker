6291195c6a1b000711b1790ecdaa75466dd14c25
백지윤 <wendy6992@naver.com>
2024-11-27 18:21:31 +0900

    Feat: Add water Shader

--- Assets/Scripts/Player/CharacterSwitchManager.cs
+++ Assets/Scripts/Player/CharacterSwitchManager.cs
@@ -1,18 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterSwitchManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public PlayerController playerController; // PlayerController 참조
+    public FemaleCharacterController femaleCharacterController; // FemaleCharacterController 참조
+    public KeyCode switchKey = KeyCode.Tab; // 캐릭터 전환 키
+
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(switchKey))
+        {
+            SwitchCharacter();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SwitchCharacter()
     {
-        
+        // 활성화 상태 전환
+        bool playerActive = playerController.isActive;
+
+        playerController.isActive = !playerActive;
+        femaleCharacterController.isActive = playerActive;
+
+        // // 애니메이션 상태 초기화 (선택 사항)
+        // if (playerController.isActive)
+        // {
+        //     playerController.anim.SetBool("walk", false);
+        //     playerController.anim.SetBool("run", false);
+        // }
+        // else if (femaleCharacterController.isActive)
+        // {
+        //     femaleCharacterController.anim.SetBool("walk", false);
+        // }
+
+        Debug.Log("Switched character. Player active: " + playerController.isActive);
     }
 }
