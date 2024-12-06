3b6e563aff43ed0eb8b959dc6adc7031d9e14f70
Starbow-Break <phsyobs@naver.com>
2024-12-06 22:21:33 +0900

    구름 전 종류에 새로운 스프라이트 및 애니메이션 적용

--- Assets/Scripts/Object/Cloud.cs
+++ Assets/Scripts/Object/Cloud.cs
@@ -9,12 +9,19 @@ public class Cloud : MonoBehaviour
     [SerializeField] float spawnTime = 2.0f; // 스폰 시간
     [SerializeField] ParticleSystem particle = null; // 파티클
     Animator anim; // 애니매이터
-    Coroutine coroutine; // 현재 실행중인 코루틴 
+    Coroutine disappearCoroutine; // 현재 실행중인 Disappear 코루틴 
+    Coroutine spawnCoroutine; // 현재 실행중인 Spawn 코루틴 
     
 
     private void Awake() {
         anim = GetComponent<Animator>();
         particle?.Stop();
+        disappearCoroutine = null;
+        spawnCoroutine = null;
+    }
+
+    private void Start() {
+        anim.SetFloat("steppingAnimSpeed", 1.0f / disappearTime);
     }
 
     // 밟으면 구름이 사라질 징조를 보여주고 일정 시간이 지난 후 사라진다.
@@ -34,15 +41,15 @@ public class Cloud : MonoBehaviour
 
     // 구름이 사라짐
     public void Disappear() {
-        if(coroutine == null) {
-            coroutine = StartCoroutine(DisappearFlow());
+        if(disappearCoroutine == null) {
+            disappearCoroutine = StartCoroutine(DisappearFlow());
         }
     }
 
     // 구름 생성
     public void Spawn() {
-        if(coroutine == null) {
-            coroutine = StartCoroutine(SpawnFlow());
+        if(spawnCoroutine == null) {
+            spawnCoroutine = StartCoroutine(SpawnFlow());
         }
     }
 
@@ -50,12 +57,12 @@ public class Cloud : MonoBehaviour
         yield return new WaitForSeconds(disappearTime);
         anim.ResetTrigger("step");
         anim.SetTrigger("disappear");
-        coroutine = null;
+        disappearCoroutine = null;
     }
 
     IEnumerator SpawnFlow() {
         yield return new WaitForSeconds(spawnTime);
         anim.SetTrigger("spawn");
-        coroutine = null;
+        spawnCoroutine = null;
     }
 }
