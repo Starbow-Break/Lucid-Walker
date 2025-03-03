d612eb47d23f1a5f1c918b673d6bebc9eddca203
Starbow-Break <phsyobs@naver.com>
2025-03-04 00:07:17 +0900

    ADD : 3페이즈 시작 전 플랫포머 페이즈 제작 중

--- Assets/Scripts/Enemy/Boss/ThreatSkill.cs
+++ Assets/Scripts/Enemy/Boss/ThreatSkill.cs
@@ -6,7 +6,7 @@ public class ThreatSkill : Skill
 {
     [SerializeField, Min(0.0f)] float duration; // 지속 시간
     [SerializeField] float velocity; // 보스의 이동 속도
-    [SerializeField] PlatformScrolling platformScrolling; // 플랫폼 스크롤러
+    [SerializeField] ObjectScrolling objectScrolling; // 플랫폼 스크롤러
 
     Animator casterAnimator;
 
@@ -16,12 +16,12 @@ public class ThreatSkill : Skill
 
     protected override IEnumerator SkillFlow() {
         casterAnimator.SetBool("move", true);
-        platformScrolling.scrollSpeed = velocity;
+        objectScrolling.scrollSpeed = velocity;
 
         yield return new WaitForSeconds(duration);
 
         casterAnimator.SetBool("move", false);
-        platformScrolling.scrollSpeed = 0.0f;
+        objectScrolling.scrollSpeed = 0.0f;
 
         yield return null;
     }
