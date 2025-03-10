787de7436de1c314f1647cc418a81fc910d5bb54
STARBOW <starbow.dev@gmail.com>
2025-03-10 17:49:04 +0900

    ADD : 점프 모션 기본 틀 구현

--- Assets/Scripts/Enemy/Boss/MaskBossPhase3.cs
+++ Assets/Scripts/Enemy/Boss/MaskBossPhase3.cs
@@ -10,6 +10,13 @@ public class MaskBossPhase3 : MonoBehaviour
     [SerializeField] Transform body;
     [SerializeField] Transform frontHand;
     [SerializeField] Transform backHand;
+    
+    [Header("Solver Targets")]
+    [SerializeField] Transform frontArmSolverTarget;
+    [SerializeField] Transform backArmSolverTarget;
+
+    private Vector3 landingFrontArmSolverPosition;
+    private Vector3 landingBackArmSolverPosition;
 
     public bool idle {
         get {
@@ -60,11 +67,20 @@ public class MaskBossPhase3 : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Alpha2)) {
             threatSkill.Cast();
         }
+        if(Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Jump();
+        }
     }
 
     // body를 dir만큼 움직인다,
     public void MoveBody(Vector3 dir) {
         body.localPosition += dir;
+    }
+
+    public void Jump()
+    {
+        StartCoroutine(JumpFlow());
     }
 
     IEnumerator IdleActionFlow() {
@@ -77,6 +93,57 @@ public class MaskBossPhase3 : MonoBehaviour
             body.localPosition = Vector3.Lerp(0.7f * Vector3.up, 1.0f * Vector3.up, Mathf.Cos(currentTime / period * 2.0f * Mathf.PI) / 2 + 0.5f);
         }
     }
+    
+    IEnumerator JumpFlow()
+    {
+        anim.SetBool("air", true);
+        yield return JumpReadyFlow();
+        yield return JumpingFlow();
+        
+    }
+    
+    // 상체를 살짝 내리면서 점프 준비
+    IEnumerator JumpReadyFlow()
+    {
+        float duration = 0.3f;
+        float time = 0.0f;
+        float startY = transform.position.y;
+        float targetY = -40.2f;
+
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+            float nextY = Mathf.Lerp(startY, targetY, time / duration);
+            transform.position = new(transform.position.x, nextY, transform.position.z);
+            frontArmSolverTarget.position = landingFrontArmSolverPosition;
+            backArmSolverTarget.position = landingBackArmSolverPosition;
+            yield return null;
+        }
+    }
+    
+    // 점프 중
+    IEnumerator JumpingFlow()
+    {
+        Vector3 initVelocity = Vector3.up * 40.0f;
+        Vector3 gravity = Vector3.down * 10.0f;
+        
+        Vector3 velocity = initVelocity;
+        
+        float duration = 1.0f;
+        float time = 0.0f;
+        while (time < duration)
+        {
+            time += Time.deltaTime;
+            velocity += gravity * Time.deltaTime;
+            transform.Translate(velocity * Time.deltaTime, Space.World);
+            if (transform.position.y - landingFrontArmSolverPosition.y < 8.0f)
+            {
+                frontArmSolverTarget.position = landingFrontArmSolverPosition;
+                backArmSolverTarget.position = landingBackArmSolverPosition;
+            }
+            yield return null;
+        }
+    }
 
     void Shake() {
         CameraShake.instance.ShakeActiveCamera(5.0f, 0.1f);
@@ -86,8 +153,14 @@ public class MaskBossPhase3 : MonoBehaviour
     {
         if (other.collider.CompareTag("Platform"))
         {
-            Shake();
-            rb.isKinematic = true;
+            if(anim.GetBool("air"))
+            {
+                anim.SetBool("air", false);
+                CameraShake.instance.ShakeActiveCamera(5.0f, 0.1f);
+                rb.isKinematic = true;
+                landingFrontArmSolverPosition = frontArmSolverTarget.position;
+                landingBackArmSolverPosition = backArmSolverTarget.position;
+            }
         }
     }
 }
