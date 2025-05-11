e00b34c7c8baf91b5c75c0d6d1b79d5322843eb9
Starbow-Break <phsyobs@naver.com>
2025-05-11 19:07:28 +0900

    ADD : 보물상자 열릴 때 효과 추가

--- Assets/Scripts/Object/TreasureBox.cs
+++ Assets/Scripts/Object/TreasureBox.cs
@@ -1,9 +1,18 @@
+using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class TreasureBox : MonoBehaviour
 {
-    [SerializeField] GameObject item;
+    [System.Serializable]
+    private struct SpawnData {
+        public GameObject item;
+        public int count;
+    }
+
+    [SerializeField] List<SpawnData> spawnDatas;
     [SerializeField] Transform spawnPoint;
+    [SerializeField, Min(0f)] float spawnBound = 0.5f;
     [SerializeField] KeyGuide keyGuide;
 
     Animator anim;
@@ -33,15 +42,38 @@ public class TreasureBox : MonoBehaviour
 
     void Open()
     {
+        
         isOpen = true;
-        anim.SetTrigger("open");
         isInteracting = false;
         keyGuide.InActive();
+        OpenSequance();
+        
+    }
+
+    void OpenSequance()
+    {
+        Sequence seq = DOTween.Sequence();
+
+        seq.Append(transform.DOShakeScale(1f, strength: new Vector3(0.05f, 0.05f), vibrato: 10));
+        seq.AppendInterval(0.2f);
+        seq.AppendCallback(() =>
+        {
+           anim.SetTrigger("open"); // 스프라이트 애니메이션
+        });
+        seq.Append(transform.DOPunchScale(new Vector3(-0.2f, 0.2f), 0.5f, vibrato: 3));
     }
 
     void SpawnItem()
     {
-        Instantiate(item, spawnPoint.position, Quaternion.identity);
+        foreach(var spawnData in spawnDatas)
+        {
+            for(int i = 0; i < spawnData.count; i++)
+            {
+                float offset = Random.Range(-spawnBound, spawnBound);
+                GameObject spawnedItem = Instantiate(spawnData.item, spawnPoint.position, Quaternion.identity);
+                spawnedItem.transform.DOLocalJump(spawnPoint.position + offset * Vector3.right, 3.0f, 1, 1.0f, false);
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
